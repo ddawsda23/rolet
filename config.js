c3ebe5ff14c120a916f88dacdac51ae030878c0b
ddawsda23 <abdullahhawsawi0077x+@gmail.com>
2026-03-16 22:11:36 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // ضع التوكن الخاص بك هنا
-  token: "YOUR_BOT_TOKEN_HERE",
+  token: "MTQ4MzE4MDcyODU0NzI4MzA0NA.GADSZf.z1-huMRCKTnij7lEcoDMeOd0oEkBMTrihCkjPs",
 
   // البادئة للأوامر
   prefix: "-",
