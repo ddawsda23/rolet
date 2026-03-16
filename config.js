4c0fbe353fa7df3d4bcfe0b33b5de7967848d5eb
ddawsda23 <abdullahhawsawi0077x+@gmail.com>
2026-03-16 22:26:51 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  // ضع التوكن الخاص بك هنا
-  token: "MTQ4MzE4MDcyODU0NzI4MzA0NA.GADSZf.z1-huMRCKTnij7lEcoDMeOd0oEkBMTrihCkjPs",
-
-  // البادئة للأوامر
+  token: process.env.TOKEN,
   prefix: "-",
 };
